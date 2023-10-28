ea0eb68d71b64298d6ff13be6f58f9b43e29a5f2
young <young20050727@gmail.com>
2023-10-28 21:04:20 +0800

    update

--- server/docker/.bashrc
+++ server/docker/.bashrc
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=randompw
 export POSTGRES_DATABASE=postgres
---
+++ server/docker/config.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+set -x POSTGRES_USER postgres
+set -x POSTGRES_PASSWORD randompw
+set -x POSTGRES_DATABASE postgres
+
+set -x MATRIX_HOSTNAME matrix.young-tw.com
+set -x REG_SHARED_SECRET secret
+set -x APP_BASE_URL https://joplin.young-tw.com
