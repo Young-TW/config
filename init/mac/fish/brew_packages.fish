698029cfa6f6edbc22c262e2efabce171d934829
young <young20050727@gmail.com>
2024-05-24 15:15:22 +0800

    update mac init script

--- init/mac/fish/brew_packages.fish
+++ init/mac/fish/brew_packages.fish
@@ -2,12 +2,12 @@
 
 # install homebrew packages
 
-for i in "btop" "clang" "cmake" "cmatrix" "cmus" "fish" "gh" "git" "java" "neofetch" "onefetch" "python3" "ranger" "tmux" "youtube-dl"
+for i in "btop" "clang" "cmake" "cmatrix" "cmus" "docker" "fish" "gh" "git" "java" "neofetch" "onefetch" "python3" "ranger" "tmux" "youtube-dl"
   eval "brew install $i"
 end
 
 # install brew cask packages
 
-for i in "anki" "anydesk" "code" "discord" "docker" "element" "firefox" "iterm2" "kitty"
+for i in "anydesk" "bitwarden" "brave-browser" "discord" "element" "iterm2" "kitty"
   eval "brew install $i"
 end
