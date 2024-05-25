252f6efea19c6ca0a5f89b1b6f3d9375a52fefe3
young <young20050727@gmail.com>
2024-05-25 10:49:04 +0800

    update

---
+++ init/config/apt/alias.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# apt alias
+
+alias ai="sudo apt install"
+alias ar="sudo apt remove"
+alias au="sudo apt update && sudo apt upgrade -y"
---
+++ init/config/dnf/alias.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# dnf alias
+
+alias di="sudo dnf install"
+alias dr="sudo dnf remove"
+alias du="sudo dnf update && sudo dnf upgrade -y"
