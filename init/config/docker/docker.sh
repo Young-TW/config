d1b5c954d7c66d5a097d1e5483ca95c8b19bcb94
young <young20050727@gmail.com>
2024-05-27 00:13:44 +0800

    update

---
+++ init/config/docker/docker.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+if [ -x "$(command -v apt)" ]; then
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+elif [ -x "$(command -v dnf)" ]; then
+    # Install Docker
+    echo "Undone Script"
+elif [ -x "$(command -v pacman)" ]; then
+    # Install Docker
+    echo "Undone Script"
+elif [ -x "$(command -v brew)" ]; then
+    brew install docker
+else
+    echo "No package manager found"
+    exit 1
+fi
