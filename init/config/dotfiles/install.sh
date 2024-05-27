db8e3b9a1b40be5b8e046db876e0231bcc59bbd8
young <young20050727@gmail.com>
2024-05-27 14:04:16 +0800

    update

---
+++ init/config/dotfiles/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# check git is installed
+
+if [ -x "$(command -v git)" ]; then
+    cd ~ || exit 1
+    # Install dotfiles
+    git clone https://github.com/Young-TW/dotfiles.git
+    mv dotfiles/home/.config ~/.config
+else
+    echo "Git is not installed"
+    exit 1
+fi
