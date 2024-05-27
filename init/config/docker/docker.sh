95d4006588518985b6ce367d427abf92f5d30fdf
young <young20050727@gmail.com>
2024-05-27 11:36:22 +0800

    update

--- init/config/docker/docker.sh
+++ init/config/docker/docker.sh
@@ -20,7 +20,10 @@ if [ -x "$(command -v apt)" ]; then
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 elif [ -x "$(command -v dnf)" ]; then
     # Install Docker
-    echo "Undone Script"
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
 elif [ -x "$(command -v pacman)" ]; then
     # Install Docker
     echo "Undone Script"
