c06ead657e0e0d6ba38c6a9183efa012e9ccba9c
young <young20050727@gmail.com>
2024-05-27 11:37:10 +0800

    update

--- server/docker/config.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-set -x POSTGRES_USER postgres
-set -x POSTGRES_PASSWORD randompw
-set -x POSTGRES_DATABASE postgres
-
-set -x MATRIX_HOSTNAME matrix.young-tw.com
-set -x REG_SHARED_SECRET secret
-set -x APP_BASE_URL https://joplin.young-tw.com
--- server/fish/env.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-set MATRIX_HOSTNAME "matrix.young-tw.com"
-set REG_SHARED_SECRET "secret"
-set PRSTGRES_USER "postgres"
-set POSTGRES_PASSWORD "postgres"
