d9982c2365f250a5f39440e7aac03be0b7456c66
young <young20050727@gmail.com>
2024-07-22 16:29:57 +0800

    update format.sh

--- init/desktop/common/format.fish
+++ init/desktop/common/format.fish
@@ -1,17 +1,32 @@
 #!/usr/bin/env fish
 
-# check argument is cpp or sh
+# check argument is cpp, rust, sh, bash, or fish
 
 if test (count $argv) -eq 0
-    echo "Usage: format.sh <cpp|sh>"
+    echo "Usage: format.sh <cpp|rust|sh|bash|fish>"
     exit 1
 end
 
-if test $argv[1] = "cpp"
-    find . -name '*.cpp' -exec clang-format -i {} \;
-else if test $argv[1] = "sh"
-    find . -name '*.sh' | while read -l file; shfmt -w "$file"; end
-else
-    echo "Usage: format.sh <cpp|sh>"
-    exit 1
+set format_type $argv[1]
+
+switch $format_type
+    case "cpp"
+        for ext in cpp hpp c h cc cxx hxx
+            find . -name "*.$ext" -exec clang-format -i {} \;
+        end
+    case "rust"
+        find . -name '*.rs' | while read -l file
+            rustfmt "$file"
+        end
+    case "sh" "bash"
+        find . -name '*.sh' | while read -l file
+            shfmt -w "$file"
+        end
+    case "fish"
+        find . -name '*.fish' | while read -l file
+            fish_indent -w "$file"
+        end
+    case '*'
+        echo "Usage: format.sh <cpp|rust|sh|bash|fish>"
+        exit 1
 end
